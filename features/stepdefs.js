8e430162394c95b53511877533d5514f849f8b7f
Victor <victor.j.zhao@gmail.com>
2023-03-03 11:26:47 -0500

    wrote F11 and F12

--- features/stepdefs.js
+++ features/stepdefs.js
@@ -1,29 +1,30 @@
-const assert = require('assert');
-const { Given, When, Then } = require('@cucumber/cucumber');
+const assert = require("assert");
+const { Given, When, Then } = require("@cucumber/cucumber");
 //import chai
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../app');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+//const app = require("../app");
 const should = chai.should();
 chai.use(chaiHttp);
 
-Given('a student with id {string}', function (id) {
-    this.id = id;
+Given("a student with id {string}", function (id) {
+  this.id = id;
 });
 
-When('a student requests to get the todo with id {string}', async function (id) {
+When(
+  "a student requests to get the todo with id {string}",
+  async function (id) {
     const response = await this.request.get(`/todos/${id}`);
     this.response = response;
+  }
+);
+
+Then("the student should get the todo with id {string}", function (id) {
+  this.response.body.should.have.property("id").eql(id);
 });
 
-Then('the student should get the todo with id {string}', function (id) {
-    this.response.body.should.have.property('id').eql(id);
-});
-
-Then('the student should get the todo with title {string}', function (title) {
-    this.response.body.should.have.property('title').eql(title);
+Then("the student should get the todo with title {string}", function (title) {
+  this.response.body.should.have.property("title").eql(title);
 });
 
 //This is all bullshit code generated be ChatGPT
-
-
